bbf77d98a4848bd84114a62c1bb1cf8141d8a34c
Cristóvão da Cruz e Silva <CrisXed@gmail.com>
2025-07-01 14:26:29 +0100

    Added an entry point for plotting the hit rate

--- src/sampiclyser/entry_points.py
+++ src/sampiclyser/entry_points.py
@@ -27,6 +27,7 @@ This module defines top-level functions that can be used as console_scripts
 entry points or imported directly into other Python code.
 """
 
+import datetime
 from pathlib import Path
 
 import click
@@ -131,6 +132,124 @@ def plot_hits(
         df=hit_summary,
         first_channel=first,
         last_channel=last,
+        cms_label=cms_label,
+        log_y=log_y,
+        figsize=(fig_width, fig_height),
+        rlabel=rlabel,
+        is_data=is_data,
+        title=title,
+    )
+
+    if output:
+        fig.savefig(output)
+    else:
+        plt.show()
+
+
+@cli.command()
+@click.argument('decoded_file', type=click.Path(exists=True, path_type=Path))
+@click.option('--bin-size', '-b', type=float, default=0.5, help='Bin size in seconds for hit-rate histogram. Default: 0.5 s')
+@click.option('--output', '-o', type=click.Path(), help='Path to save the plot image')
+@click.option('--plot-hits', 'plot_hits', is_flag=True, help='Enable hit per bin plotting instead of rate per bin')
+@click.option(
+    "--start-time",
+    "start_time",
+    type=click.DateTime(formats=["%Y-%m-%dT%H:%M:%S"]),
+    help="Overrirde run start time in ISO format, e.g. '2025-06-20T17:45:32'.",
+)
+@click.option(
+    "--end-time",
+    "end_time",
+    type=click.DateTime(formats=["%Y-%m-%dT%H:%M:%S"]),
+    help="Overrirde run end time in ISO format, e.g. '2025-06-20T18:00:00'.",
+)
+@click.option(
+    '--root-tree',
+    'root_tree',
+    type=str,
+    default="sampic_hits",
+    help='The name of the root ttree under which to save the hit data. Default: sampic_hits',
+)
+@click.option(
+    '--scale-factor',
+    'scale_factor',
+    type=float,
+    default=1.0,
+    help='The scale factor to apply to the hit count, used for adjusting central trigger. Default: 1.0',
+)
+@click.option(
+    '--cms-label',
+    'cms_label',
+    type=str,
+    default="Preliminary",
+    help='The plot label to put near the CMS text. Default: Preliminary',
+)
+@click.option('--logy', 'log_y', is_flag=True, help='Enable logarithmic y axis')
+@click.option(
+    '--fig-width',
+    'fig_width',
+    type=float,
+    default=15,
+    help='The width of the plot. Default: 15',
+)
+@click.option(
+    '--fig-height',
+    'fig_height',
+    type=float,
+    default=9,
+    help='The height of the plot. Default: 9',
+)
+@click.option(
+    '--right-label',
+    'rlabel',
+    type=str,
+    default="Test",
+    help='The plot label to put on the right side of the figure, typically the beam details. Default: Test',
+)
+@click.option(
+    '--is-data',
+    '-d',
+    'is_data',
+    is_flag=True,
+    help='Whether the processed data corresponds to real data (as an alternative to simulation data)',
+)
+@click.option(
+    '--title',
+    '-t',
+    'title',
+    type=str,
+    default=None,
+    help='The plot title to put at the top of the figure. Default: None',
+)
+def plot_hit_rate(
+    decoded_file: Path,
+    bin_size: float,
+    output: Path,
+    plot_hits: bool,
+    start_time: datetime.datetime,
+    end_time: datetime.datetime,
+    root_tree: str,
+    scale_factor: float,
+    cms_label: str,
+    log_y: bool,
+    fig_width: float,
+    fig_height: float,
+    rlabel: str,
+    is_data: bool,
+    title: str,
+):
+    """
+    Plot hit rate vs. time from a decoded SAMPIC run file.
+    """
+
+    fig = sampiclyser.plot_hit_rate(
+        file_path=decoded_file,
+        bin_size=bin_size,
+        plot_hits=plot_hits,
+        start_time=start_time,
+        end_time=end_time,
+        root_tree=root_tree,
+        scale_factor=scale_factor,
         cms_label=cms_label,
         log_y=log_y,
         figsize=(fig_width, fig_height),
